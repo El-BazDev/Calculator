a3670e377940010fe6b3847d76d1b59c9e4163ed
El_BazDev <124913839+El-BazDev@users.noreply.github.com>
2025-02-11 21:55:01 +0100

    Update DigiButton.js

--- calculator/src/Components/Button/DigiButton.js
+++ calculator/src/Components/Button/DigiButton.js
@@ -1,14 +1,16 @@
 import { useDispatch } from 'react-redux'
 import { ADD_DEGIT } from '../../Redux/CalculateSlice'
 
-
-function DigiButton({digit,theme}) {
+function DigiButton({digit, theme}) {
     const dispatch = useDispatch()
-
-  return (
-    
-    <button className={theme==1?'keys':theme==2?'keys2':'keys3'} onClick={()=>dispatch(ADD_DEGIT({digit:digit}))}>{digit}</button>
-  )
+    return (
+        <button 
+            className={theme === 1 ? 'keys' : theme === 2 ? 'keys2' : 'keys3'} 
+            onClick={() => dispatch(ADD_DEGIT({digit: digit}))}
+        >
+            {digit}
+        </button>
+    )
 }
 
 export default DigiButton
